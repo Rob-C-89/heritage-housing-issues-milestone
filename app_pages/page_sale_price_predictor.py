885c0508c3a0655f14d699c2283bee21250c61e6
Rob-C-89 <robbiecornell89@gmail.com>
2026-05-13 13:43:57 +0000

    Write draw input widgets function

--- app_pages/page_sale_price_predictor.py
+++ app_pages/page_sale_price_predictor.py
@@ -8,9 +8,13 @@ import seaborn as sns
 import pandas as pd
 sns.set_style("whitegrid")
 
+from src.data_management import load_pkl_file, load_house_data
+
 def page_sale_price_predictor_body():
     # Load predict sale price files
     inherited_predictions = pd.read_csv(f"outputs/ml_pipeline/predict_saleprice/inherited_house_predictions.csv")
+    pipeline = load_pkl_file("outputs/ml_pipeline/predict_saleprice/extra_trees_pipeline.pkl")
+
 
     # State BR2
     st.write("# Sale Price Predictor")
@@ -30,7 +34,15 @@ def page_sale_price_predictor_body():
     )
     display_inherited_predictions(inherited_predictions)
 
-    # Widgets to predict sale price on any given property
+    # Live interface for predicting sales price
+    st.write("## Predict Sales Price Interface")
+    st.info("Input house features in the below widgets to see a predicted sale price for any given property.")
+
+    X_live = draw_input_widgets()
+
+    #if st.button("Run sales price prediction"):
+
+
 
     # House price display
 
@@ -44,3 +56,81 @@ def display_inherited_predictions(inherited_predictions):
             'TotalBsmtSF': '{:.0f}',
         })
     )
+
+def draw_input_widgets():
+    df = load_house_data()
+    percentageMin, percentageMax = 0.4, 2.0
+
+    X_live = pd.DataFrame([], index=[0])
+
+    col1, col2, col3 = st.columns(3)
+    col4, col5, col6 = st.columns(3)
+    col7, col8, col9 = st.columns(3)
+
+    with col1:
+        feature = "OverallQual"
+        st_widget = st.selectbox(
+            label=feature,
+            options=sorted(df[feature].unique())
+        )
+        X_live[feature] = st_widget
+
+    with col2:
+        feature = "GrLivArea"
+        st_widget = st.number_input(
+            label=feature,
+            min_value=df[feature].min()*percentageMin,
+            max_value=df[feature].max()*percentageMax,
+            value=df[feature].median()
+        )
+        X_live[feature] = st_widget
+
+    with col3:
+        feature = "KitchenQual"
+        st_widget = st.selectbox(
+            label=feature,
+            options=df[feature].unique()
+        )
+        X_live[feature] = st_widget
+
+    with col4:
+        feature = "YearBuilt"
+        st_widget = st.number_input(
+            label=feature,
+            min_value=df[feature].min()*percentageMin,
+            max_value=df[feature].max()*percentageMax,
+            value=df[feature].median()
+        )
+        X_live[feature] = st_widget
+
+    with col5:
+        feature = "GarageArea"
+        st_widget = st.number_input(
+            label=feature,
+            min_value=df[feature].min()*percentageMin,
+            max_value=df[feature].max()*percentageMax,
+            value=df[feature].median()
+        )
+        X_live[feature] = st_widget
+
+    with col6:
+        feature = "1stFlrSF"
+        st_widget = st.number_input(
+            label=feature,
+            min_value=df[feature].min()*percentageMin,
+            max_value=df[feature].max()*percentageMax,
+            value=df[feature].median()
+        )
+        X_live[feature] = st_widget
+
+    with col7:
+        feature = "TotalBsmtSF"
+        st_widget = st.number_input(
+            label=feature,
+            min_value=df[feature].min()*percentageMin,
+            max_value=df[feature].max()*percentageMax,
+            value=df[feature].median()
+        )
+        X_live[feature] = st_widget
+
+    return X_live
