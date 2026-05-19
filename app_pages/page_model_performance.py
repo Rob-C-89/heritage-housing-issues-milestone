5624c5e6af022a7db0f4d6e580598bb7bed6dbab
Rob-C-89 <robbiecornell89@gmail.com>
2026-05-19 10:10:46 +0000

    Add Model Performance and Regression Evaluation

--- app_pages/page_model_performance.py
+++ app_pages/page_model_performance.py
@@ -35,10 +35,41 @@ def page_model_performance_body():
     display_pipeline_steps(pipeline)
 
     # Feature importance (graph)
+    st.write("### Feature Importance")
+    st.write("" \
+    "The most important features for predicting the sale price of a house were: "
+    "OverallQuality, GrLivArea, KitchenQual, YearBuilt, GarageArea, 1stFlrSF and TotalBsmtSF. \n \n"
+    "The model was trained on these features. The diagram below displays the relative importance "
+    "of each feature. \n \n")
+
+    st.image("outputs/plots/feature_importance.png")
 
     # Model performance: R² and RMSE on train and test sets (table), confirmation (text)
+    st.write(
+        "### Model Performance  \n \n"
+        "The model results from the train and test set are as follows:"
+    )
+    st.markdown("""
+        | Metric | Train Set | Test Set |
+        |---|---|---|
+        | **R² Score** | 0.943 | 0.880 |
+        | **Mean Absolute Error** | 12,313.615 | 18,615.871 |
+        | **Mean Squared Error** | 337,456,356.691 | 923,834,197.056 |
+        | **Root Mean Squared Error** | 18,369.985 | 30,394.641 |
+
+        The R2 score from the test set is 0.88, which is well above the 0.75 defined in our rationale.
+
+        The R2 score from the train set is 0.943, with a variance of 0.063 from the test set.
+    """)
 
     # Predicted vs Actual Sale Price plot (graph)
+    st.write(
+        "### Predicted vs. Actual Sale Price  \n \n"
+        "After training, we parsed 20 percent of the housing data to the model, withholding the sale price. \n \n"
+        "The graph below shows the model's predicted sale price against the actual sale price on this test set." \
+    )
+
+    st.image("outputs/plots/regression_evaluation.png")
 
     # Considerations and conclusions after the pipeline is trained (text)
 
